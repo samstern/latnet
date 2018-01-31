13f4a941381077c251085197b8be593664181ddc
samstern <samuelostern@gmail.com>
2018-01-31 12:08:55 +0000

    loader conn not class attribute

--- latnet/loader.py
+++ latnet/loader.py
@@ -8,7 +8,7 @@ class Loader(object):
 
 	def db_connect(self,dbname,username,password):
 		"""sets up a connection with a postgresql database using psycopg2"""
-		self.conn=psycopg2.connect(database=dbname,user=username,password=password)
+		conn=psycopg2.connect(database=dbname,user=username,password=password)
 		self.cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)		
 
 	def executeQuery(self,query_file,*args):
