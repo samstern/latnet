665dab23ad27bb1e8852e000a24ef359bd2780c0
samstern <samuelostern@gmail.com>
2018-02-01 15:27:10 +0000

    name now identifier

--- latnet/agent.py
+++ latnet/agent.py
@@ -1,8 +1,7 @@
 class Agent(object):
 	"""Agents are the nodes in the network"""
-	def __init__(self,id,name):
-		self.id=id
-		self.name=name
+	def __init__(self,identifier):
+		self.identifier=identifier
 
 	def update(self):
 		pass
@@ -10,9 +9,9 @@ class Agent(object):
 		
 class TopicSentimentAgent(Agent):
 	"""An Agent that has a sentiment score for various topics"""
-	def __init__(self, id, name):
-		super(TopicSentimentAgent, self).__init__(id,name)
+	def __init__(self, identifier):
+		super(TopicSentimentAgent, self).__init__(identifier)
 		self.topic_sentiment=dict()
-		
+
 	def update(self,topic,sentiment):
 		self.topic_sentiment[topic]=sentiment
